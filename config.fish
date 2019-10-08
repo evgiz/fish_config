6b28568e27f3ec91ac5b62eae1f1ccd9ae0e111d
sigve <me@evgiz.net>
2019-10-08 13:20:32 +0200

    Simple fish config

---
+++ config.fish
@@ -0,0 +1,35 @@
+# Git abbreviations
+abbr -a -g g git
+abbr -a -g ga git add
+abbr -a -g gaa git add --all
+abbr -a -g gcmsg git commit -m
+abbr -a -g gc git commit -m
+abbr -a -g gp git push
+abbr -a -g gl git pull
+abbr -a -g gst git status
+abbr -a -g glog git log --oneline --decorate --color --graph
+abbr -a -g gco git checkout
+abbr -a -g gba git branch -a
+abbr -a -g gb git branch
+abbr -a -g gcm git checkout master
+
+# Destinations
+abbr -a -g dev $HOME/Development/
+abbr -a -g stud $HOME/Studies/
+abbr -a -g desk $HOME/Desktop/
+abbr -a -g samf $HOME/Development/Samfundet/Samfundet
+
+# Python
+abbr -a -g pip pip3
+abbr -a -g pip2 pip
+abbr -a -g py python3
+abbr -a -g py2 python
+
+# Greeting
+set fish_greeting "  ______ _____  _____ _    _ 
+|  ____|_   _|/ ____| |  | |
+| |__    | | | (___ | |__| |
+|  __|   | |  \___ \|  __  |
+| |     _| |_ ____) | |  | |
+|_|    |_____|_____/|_|  |_|
+"

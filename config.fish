c1031bf9d442cb6b51b30315318c7fdc319c3856
sigve <me@evgiz.net>
2019-10-08 13:57:52 +0200

    Added mac application shortcuts

--- config.fish
+++ config.fish
@@ -1,3 +1,12 @@
+# Greeting
+set fish_greeting " ______ _____  _____ _    _ 
+|  ____|_   _|/ ____| |  | |
+| |__    | | | (___ | |__| |
+|  __|   | |  \___ \|  __  |
+| |     _| |_ ____) | |  | |
+|_|    |_____|_____/|_|  |_|
+"
+
 # Git abbreviations
 abbr -a -g g git
 abbr -a -g ga git add
@@ -12,6 +21,7 @@ abbr -a -g gco git checkout
 abbr -a -g gba git branch -a
 abbr -a -g gb git branch
 abbr -a -g gcm git checkout master
+abbr -a -g gd git diff
 
 # Destinations
 abbr -a -g dev $HOME/Development/
@@ -20,16 +30,57 @@ abbr -a -g desk $HOME/Desktop/
 abbr -a -g samf $HOME/Development/Samfundet/Samfundet
 
 # Python
-abbr -a -g pip pip3
-abbr -a -g pip2 pip
 abbr -a -g py python3
+abbr -a -g pip pip3
 abbr -a -g py2 python
+abbr -a -g pip2 pip
 
-# Greeting
-set fish_greeting "  ______ _____  _____ _    _ 
-|  ____|_   _|/ ____| |  | |
-| |__    | | | (___ | |__| |
-|  __|   | |  \___ \|  __  |
-| |     _| |_ ____) | |  | |
-|_|    |_____|_____/|_|  |_|
-"
+
+# ==================== #
+#  Applications (Mac)  #
+# ==================== #
+
+# VS Code
+function code 
+    if count $argv > /dev/null
+        open $argv -a "/Applications/Visual Studio Code.app" 
+    else
+        open . -a "/Applications/Visual Studio Code.app"
+    end
+end
+
+# PyCharm
+function pycharm 
+    if count $argv > /dev/null
+        open $argv -a "/Applications/PyCharm CE.app" 
+    else
+        open . -a "/Applications/PyCharm CE.app"
+    end
+end
+
+# RubyMine
+function rubymine 
+    if count $argv > /dev/null
+        open $argv -a "/Applications/RubyMine.app" 
+    else
+        open . -a "/Applications/RubyMine.app"
+    end
+end
+
+# CLion
+function clion 
+    if count $argv > /dev/null
+        open $argv -a "/Applications/CLion.app" 
+    else
+        open . -a "/Applications/CLion.app"
+    end
+end
+
+# Love2D
+function love
+ if count $argv > /dev/null
+        open $argv -a "/Applications/love.app" 
+    else
+        open . -a "/Applications/love.app"
+    end
+end
